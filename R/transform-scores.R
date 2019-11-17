4ca4d45d1ec7eb0511bc8059fa03ab8290f3fa5f
IsaacVerm <isaacverm@gmail.com>
2019-11-17 23:44:19 +0100

    add add_goals_home(), add_goals_away() and add_goal_difference()

--- R/transform-scores.R
+++ R/transform-scores.R
@@ -6,3 +6,25 @@ scores_to_df <- function(list_scores) {
                  score = scores_season)
     })
 }
+
+#' @export
+add_goals_home <- function(df_scores) {
+  df_scores %>%
+    dplyr::mutate(goals_home = stringr::str_extract_all(score,
+                                                        "\\d+",
+                                                        simplify = TRUE)[, 1] %>% as.integer)
+}
+
+#' @export
+add_goals_away <- function(df_scores) {
+  df_scores %>%
+    dplyr::mutate(goals_away = stringr::str_extract_all(score,
+                                                        "\\d+",
+                                                        simplify = TRUE)[, 2] %>% as.integer)
+}
+
+#' @export
+add_goal_difference <- function(df_scores) {
+  df_scores %>%
+    dplyr::mutate(goal_difference = goals_home - goals_away)
+}
