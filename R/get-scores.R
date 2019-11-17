7d0207bb3a3166bf74acf04a2dad6724061bb3df
IsaacVerm <isaacverm@gmail.com>
2019-11-17 20:40:09 +0100

    add get_seasons()

--- R/get-scores.R
+++ R/get-scores.R
@@ -1,3 +1,19 @@
+get_seasons <- function() {
+  c("2007–08",
+    "2008–09",
+    "2009–10",
+    "2010–11",
+    "2011–12",
+    "2012–13",
+    "2013–14",
+    "2014–15",
+    "2015–16",
+    "2016–17",
+    "2017–18",
+    "2018–19",
+    "2019–20")
+}
+
 create_wiki_jpl_url <- function(season) {
   start_year <- substr(season, 1, 4)
   end_year <- substr(season, 6, 8)
