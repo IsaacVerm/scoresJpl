ceae1fca017329ae92e20096bb43fe40ab9c64a8
IsaacVerm <isaacverm@gmail.com>
2019-11-17 23:57:31 +0100

    update aggregate_scores_by_frequency() to use proportion instead of freq

--- R/aggregate-scores.R
+++ R/aggregate-scores.R
@@ -3,5 +3,5 @@ aggregate_scores_by_frequency <- function(df_scores) {
   df_scores %>%
     dplyr::group_by(score) %>%
     dplyr::summarise(n = n()) %>%
-    dplyr::mutate(freq = n / sum(n)) # https://stackoverflow.com/questions/24576515/relative-frequencies-proportions-with-dplyr
+    dplyr::mutate(proportion = n / sum(n)) # https://stackoverflow.com/questions/24576515/relative-frequencies-proportions-with-dplyr
 }
