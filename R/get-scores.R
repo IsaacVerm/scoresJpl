f826cd7fed1b0cd67065241bd623229c5cbc57cd
IsaacVerm <isaacverm@gmail.com>
2019-11-17 20:27:12 +0100

    document xpath parse_scores

--- R/get-scores.R
+++ R/get-scores.R
@@ -21,6 +21,19 @@ get_html_wiki_jpl <- function(url) {
   xml2::read_html(url)
 }
 
+#' Parses the scores of the html wiki season page.
+#'
+#' \code{parse_scores}
+#'
+#' The xpath works like this:
+#'
+#' 1) Look for a Results id.
+#'
+#' 2) Go to the parent. I'm not specific here about what the parent should be like because it can be h2, h3, ...
+#'
+#' 3) Look for the next following sibling. Next because [1] is specified.
+#'
+#' 4) Get each cell of the table.
 parse_scores <- function(html) {
   html %>%
     rvest::html_nodes(xpath = '//*[@id="Results"]/../following-sibling::div[1]/div/table//td') %>%
