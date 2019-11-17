cf08cfa68e89345e18cc4830cab6db28f6575275
IsaacVerm <isaacverm@gmail.com>
2019-11-18 00:44:09 +0100

    add functions to aggregate average goals by season

--- R/aggregate-scores.R
+++ R/aggregate-scores.R
@@ -5,3 +5,22 @@ aggregate_scores_by_frequency <- function(df_scores) {
     dplyr::summarise(n = n()) %>%
     dplyr::mutate(proportion = n / sum(n)) # https://stackoverflow.com/questions/24576515/relative-frequencies-proportions-with-dplyr
 }
+
+#' @export
+aggregate_average_goals_by_season <- function(df_scores) {
+  df_scores %>%
+    dplyr::group_by(season) %>%
+    dplyr::summarise(
+      average_goals_home = mean(goals_home),
+      average_goals_away = mean(goals_away)
+    )
+}
+
+#' @export
+average_goals_by_season_to_long <- function(average_goals_by_season) {
+  average_goals_by_season %>%
+    tidyr::gather(key = "goals_home_or_away",
+                  value = "goals",
+                  average_goals_home,
+                  average_goals_away)
+}
