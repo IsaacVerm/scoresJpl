bc58c5df0de41c888617599d5c1445b7be92791a
IsaacVerm <isaacverm@gmail.com>
2019-11-17 21:47:55 +0100

    update league url/xpath matching with season

--- R/get-scores.R
+++ R/get-scores.R
@@ -1,5 +1,7 @@
 get_seasons <- function() {
-  c("2007–08",
+  c(
+    "2006–07",
+    "2007–08",
     "2008–09",
     "2009–10",
     "2010–11",
@@ -11,29 +13,63 @@ get_seasons <- function() {
     "2016–17",
     "2017–18",
     "2018–19",
-    "2019–20")
+    "2019–20"
+  )
+}
+
+get_path_league <- function(season) {
+  league_by_season = list(
+    "2006–07" = "Belgian_First_Division",
+    "2007–08" = "Belgian_First_Division",
+    "2008–09" = "Belgian_First_Division",
+    "2009–10" = "Belgian_Pro_League",
+    "2010–11" = "Belgian_Pro_League",
+    "2011–12" = "Belgian_Pro_League",
+    "2012–13" = "Belgian_Pro_League",
+    "2013–14" = "Belgian_Pro_League",
+    "2014–15" = "Belgian_Pro_League",
+    "2015–16" = "Belgian_Pro_League",
+    "2016–17" = "Belgian_First_Division_A",
+    "2017–18" = "Belgian_First_Division_A",
+    "2018–19" = "Belgian_First_Division_A",
+    "2019–20" = "Belgian_First_Division_A"
+  )
+
+  league_by_season[[season]]
+}
+
+get_xpath_score_parsing <- function(season) {
+  pattern_1 <-
+    '//*[@id="Fixtures_and_results"]/../following-sibling::div[1]//td'
+  pattern_2 <- '//*[@id="Results"]/../following-sibling::div[1]//td'
+  pattern_3 <-
+    '//*[@id="Results"]/../following-sibling::table[1]//td'
+
+  pattern_by_season = list(
+    "2006–07" = pattern_1,
+    "2007–08" = pattern_2,
+    "2008–09" = pattern_2,
+    "2009–10" = pattern_2,
+    "2010–11" = pattern_2,
+    "2011–12" = pattern_2,
+    "2012–13" = pattern_2,
+    "2013–14" = pattern_2,
+    "2014–15" = pattern_2,
+    "2015–16" = pattern_3,
+    "2016–17" = pattern_2,
+    "2017–18" = pattern_2,
+    "2018–19" = pattern_2,
+    "2019–20" = pattern_2
+  )
+
+  pattern_by_season[[season]]
 }
 
 create_wiki_jpl_url <- function(season) {
-  start_year <- substr(season, 1, 4)
-  end_year <- substr(season, 6, 8)
-
-  if (start_year > 2015) {
-    league = "_Belgian_First_Division_A"
-  }
-  else if (start_year > 2009) {
-    league = "_Belgian_Pro_League"
-  } else if (start_year > 2005) {
-    league = "_Belgian_First_Division"
-  } else {
-    stop("You're trying to create the url for a season without results data.")
-  }
-
   paste0("https://en.wikipedia.org/wiki/",
-         start_year,
-         "-",
-         end_year,
-         league)
+         season,
+         "_",
+         get_path_league(season))
 }
 
 get_html_wiki_jpl <- function(url) {
@@ -53,9 +89,9 @@ get_html_wiki_jpl <- function(url) {
 #' 3) Look for the next following sibling. Next because [1] is specified.
 #'
 #' 4) Get each cell of the table.
-parse_scores <- function(html) {
+parse_scores <- function(html, season) {
   html %>%
-    rvest::html_nodes(xpath = '//*[@id="Results"]/../following-sibling::div[1]//td') %>%
+    rvest::html_nodes(xpath = get_xpath_score_parsing(season)) %>%
     rvest::html_text() %>%
     stringr::str_extract("\\d+–\\d+")
 }
@@ -67,6 +103,6 @@ get_scores <- function(seasons) {
       season %>%
         create_wiki_jpl_url() %>%
         get_html_wiki_jpl() %>%
-        parse_scores()
+        parse_scores(season)
     })
 }
