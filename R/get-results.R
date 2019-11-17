92397f367a7d910fbe70fb8b9b75e26014ec402c
IsaacVerm <isaacverm@gmail.com>
2019-11-17 19:44:18 +0100

    update create_wiki_jpl_url() to change end url depending on season

--- R/get-results.R
+++ R/get-results.R
@@ -1,7 +1,20 @@
 create_wiki_jpl_url <- function(season) {
+  start_year <- substr(season, 1, 4)
+  end_year <- substr(season, 6, 8)
+
+  if (start_year > 2009) {
+    league = "_Belgian_Pro_League"
+  } else if (start_year > 2005) {
+    league = "_Belgian_First_Division"
+  } else {
+    stop("You're trying to create the url for a season without results data.")
+  }
+
   paste0("https://en.wikipedia.org/wiki/",
-         season,
-         "_Belgian_Pro_League")
+         start_year,
+         "-",
+         end_year,
+         league)
 }
 
 get_html_wiki_jpl <- function(url) {
@@ -9,5 +22,6 @@ get_html_wiki_jpl <- function(url) {
 }
 
 parse_results <- function(html) {
-
+  html %>%
+    html_nodes(xpath = "")
 }
