3c79d5a996cd2c579f34844fa5179cc4e54ed9b5
IsaacVerm <isaacverm@gmail.com>
2019-11-17 23:54:43 +0100

    add aggregate_scores_by_frequency()

---
+++ R/aggregate-scores.R
@@ -0,0 +1,7 @@
+#' @export
+aggregate_scores_by_frequency <- function(df_scores) {
+  df_scores %>%
+    dplyr::group_by(score) %>%
+    dplyr::summarise(n = n()) %>%
+    dplyr::mutate(freq = n / sum(n)) # https://stackoverflow.com/questions/24576515/relative-frequencies-proportions-with-dplyr
+}
