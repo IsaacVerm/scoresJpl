013db536684bbcbaad91a01d7694cb9cdf6d1b5c
IsaacVerm <isaacverm@gmail.com>
2019-11-17 15:18:59 +0100

    add create_wiki_jpl_url()

---
+++ R/get-results.R
@@ -0,0 +1,13 @@
+create_wiki_jpl_url <- function(season) {
+  paste0("https://en.wikipedia.org/wiki/",
+         season,
+         "_Belgian_Pro_League")
+}
+
+get_html_wiki_jpl <- function(url) {
+
+}
+
+parse_results <- function(html) {
+
+}
