9bace4ca55b2087a0abc277f230adca958a81980
IsaacVerm <isaacverm@gmail.com>
2019-11-18 10:44:09 +0100

    add aggregate_frequency_score_by_season()

--- R/aggregate-scores.R
+++ R/aggregate-scores.R
@@ -24,3 +24,12 @@ average_goals_by_season_to_long <- function(average_goals_by_season) {
                   average_goals_home,
                   average_goals_away)
 }
+
+#' @export
+aggregate_frequency_score_by_season <- function(df_scores) {
+  df_scores %>%
+    dplyr::group_by(season, score) %>%
+    dplyr::summarise(n = n()) %>%
+    dplyr::mutate(proportion = n / sum(n))
+}
+
